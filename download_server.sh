1b059fe952856fc0788a7830e94bd8097000fd5e
iMontouch <imontouch@gmail.com>
2016-09-28 15:24:26 +0200

    improved script

--- download_server.sh
+++ download_server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+mv data data2
 wget https://www.factorio.com/get-download/0.14.9/headless/linux64 -O factorio.0.14.9.tar.gz
 tar -xzf factorio.0.14.9.tar.gz
 mv factorio/* .
+mv data2/* data/
+rm -r data2
 rm -r factorio
